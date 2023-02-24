2543e4e953e4ebcabb478a7b0967422b1c01ee68
anabel17gr <101172984+anabel17gr@users.noreply.github.com>
2023-02-24 12:33:08 +0100

    Revert "actualizamos sql"

--- contemporanicabbdd.sql
+++ contemporanicabbdd.sql
@@ -3,19 +3,13 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 24-02-2023 a las 12:18:14
+-- Tiempo de generación: 16-02-2023 a las 13:34:39
 -- Versión del servidor: 10.4.25-MariaDB
 -- Versión de PHP: 8.1.10
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Base de datos: `contemporanica`
@@ -33,8 +27,6 @@ DROP TABLE IF EXISTS `compositor`;
 CREATE TABLE IF NOT EXISTS `compositor` (
   `id_compositor` int(11) NOT NULL AUTO_INCREMENT,
   `nombre` varchar(25) NOT NULL,
-  `email` varchar(150) NOT NULL,
-  `password` varchar(150) NOT NULL,
   `biografia` varchar(150) NOT NULL,
   PRIMARY KEY (`id_compositor`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
@@ -114,7 +106,3 @@ CREATE TABLE IF NOT EXISTS `pieza_instrumento` (
 -- RELACIONES PARA LA TABLA `pieza_instrumento`:
 --
 COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
