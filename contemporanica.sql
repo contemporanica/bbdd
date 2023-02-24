f306938312cd9a647100ebfffaec1d339ae7113d
anabel17gr <anabel17gr@gmail.com>
2023-02-24 12:20:40 +0100

    actualizacion sql

--- contemporanica.sql
+++ contemporanica.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 24-02-2023 a las 12:08:36
+-- Tiempo de generación: 24-02-2023 a las 12:18:14
 -- Versión del servidor: 10.4.25-MariaDB
 -- Versión de PHP: 8.1.10
 
@@ -20,6 +20,8 @@ SET time_zone = "+00:00";
 --
 -- Base de datos: `contemporanica`
 --
+CREATE DATABASE IF NOT EXISTS `contemporanica` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `contemporanica`;
 
 -- --------------------------------------------------------
 
@@ -27,12 +29,14 @@ SET time_zone = "+00:00";
 -- Estructura de tabla para la tabla `compositor`
 --
 
-CREATE TABLE `compositor` (
-  `id_compositor` int(11) NOT NULL,
+DROP TABLE IF EXISTS `compositor`;
+CREATE TABLE IF NOT EXISTS `compositor` (
+  `id_compositor` int(11) NOT NULL AUTO_INCREMENT,
   `nombre` varchar(25) NOT NULL,
   `email` varchar(150) NOT NULL,
   `password` varchar(150) NOT NULL,
-  `biografia` varchar(150) NOT NULL
+  `biografia` varchar(150) NOT NULL,
+  PRIMARY KEY (`id_compositor`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
@@ -45,9 +49,11 @@ CREATE TABLE `compositor` (
 -- Estructura de tabla para la tabla `familia`
 --
 
-CREATE TABLE `familia` (
-  `id_familia` int(11) NOT NULL,
-  `nombre` varchar(30) NOT NULL
+DROP TABLE IF EXISTS `familia`;
+CREATE TABLE IF NOT EXISTS `familia` (
+  `id_familia` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(30) NOT NULL,
+  PRIMARY KEY (`id_familia`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
@@ -60,10 +66,12 @@ CREATE TABLE `familia` (
 -- Estructura de tabla para la tabla `instrumento`
 --
 
-CREATE TABLE `instrumento` (
-  `id_instrumento` int(11) NOT NULL,
+DROP TABLE IF EXISTS `instrumento`;
+CREATE TABLE IF NOT EXISTS `instrumento` (
+  `id_instrumento` int(11) NOT NULL AUTO_INCREMENT,
   `nombre` varchar(50) NOT NULL,
-  `id_familia` int(11) NOT NULL
+  `id_familia` int(11) NOT NULL,
+  PRIMARY KEY (`id_instrumento`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
@@ -76,11 +84,13 @@ CREATE TABLE `instrumento` (
 -- Estructura de tabla para la tabla `pieza`
 --
 
-CREATE TABLE `pieza` (
-  `id_pieza` int(11) NOT NULL,
+DROP TABLE IF EXISTS `pieza`;
+CREATE TABLE IF NOT EXISTS `pieza` (
+  `id_pieza` int(11) NOT NULL AUTO_INCREMENT,
   `id_compositor` int(11) NOT NULL,
   `nombre` varchar(100) NOT NULL,
-  `datos` varchar(100) NOT NULL
+  `datos` varchar(100) NOT NULL,
+  PRIMARY KEY (`id_pieza`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
@@ -93,76 +103,16 @@ CREATE TABLE `pieza` (
 -- Estructura de tabla para la tabla `pieza_instrumento`
 --
 
-CREATE TABLE `pieza_instrumento` (
+DROP TABLE IF EXISTS `pieza_instrumento`;
+CREATE TABLE IF NOT EXISTS `pieza_instrumento` (
   `id_pieza` int(11) NOT NULL,
-  `id_instrumento` int(11) NOT NULL
+  `id_instrumento` int(11) NOT NULL,
+  PRIMARY KEY (`id_pieza`,`id_instrumento`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- RELACIONES PARA LA TABLA `pieza_instrumento`:
 --
-
---
--- Índices para tablas volcadas
---
-
---
--- Indices de la tabla `compositor`
---
-ALTER TABLE `compositor`
-  ADD PRIMARY KEY (`id_compositor`);
-
---
--- Indices de la tabla `familia`
---
-ALTER TABLE `familia`
-  ADD PRIMARY KEY (`id_familia`);
-
---
--- Indices de la tabla `instrumento`
---
-ALTER TABLE `instrumento`
-  ADD PRIMARY KEY (`id_instrumento`);
-
---
--- Indices de la tabla `pieza`
---
-ALTER TABLE `pieza`
-  ADD PRIMARY KEY (`id_pieza`);
-
---
--- Indices de la tabla `pieza_instrumento`
---
-ALTER TABLE `pieza_instrumento`
-  ADD PRIMARY KEY (`id_pieza`,`id_instrumento`);
-
---
--- AUTO_INCREMENT de las tablas volcadas
---
-
---
--- AUTO_INCREMENT de la tabla `compositor`
---
-ALTER TABLE `compositor`
-  MODIFY `id_compositor` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT de la tabla `familia`
---
-ALTER TABLE `familia`
-  MODIFY `id_familia` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT de la tabla `instrumento`
---
-ALTER TABLE `instrumento`
-  MODIFY `id_instrumento` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT de la tabla `pieza`
---
-ALTER TABLE `pieza`
-  MODIFY `id_pieza` int(11) NOT NULL AUTO_INCREMENT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
